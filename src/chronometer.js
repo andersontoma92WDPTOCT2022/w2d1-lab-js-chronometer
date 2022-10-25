993f1148240513c8b815dbe0117e97f30c73f395
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-10-25 14:57:13 -0300

    Done , com bonus 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,58 @@
+//feito com Arash, Michela, Luana
+
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    // printTimeCallback é uma função q vem do index.js
+    // necessário checar se foi passada
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      //checar se existe o printTimeCallback
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value.toString().padStart(2, '0');
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
+
+getCentiseconds(){
+  return (this.currentTime % 60) % ;
+}
 
   split() {
     // ... your code goes here
+    let minut = this.computeTwoDigitNumber(this.getMinutes());
+    let second = this.computeTwoDigitNumber(this.getSeconds());
+    //console.log(`${minut}:${second}`);
+    return `${minut}:${second}`;
   }
 }
